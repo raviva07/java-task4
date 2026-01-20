6c297e639094fb3bf64e6258ef58138e04fee908
raviva07 <ravivarma7939@gmail.com>
2026-01-20 12:53:39 +0530

    Create AgeNotWithinRangeException.java

---
+++ AgeNotWithinRangeException.java
@@ -0,0 +1,11 @@
+class AgeNotWithinRangeException extends Exception {
+    public AgeNotWithinRangeException(String message) {
+        super(message);
+    }
+}
+
+class NameNotValidException extends Exception {
+    public NameNotValidException(String message) {
+        super(message);
+    }
+}
